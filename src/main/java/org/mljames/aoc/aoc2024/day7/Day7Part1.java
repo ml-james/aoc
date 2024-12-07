7bf2073e4c96f5b22d7218bde55cf19393f2b806
matthewjames <matthew.james@lmax.com>
2024-12-07 01:20:20 -0500

    Refactor Day7Part1

--- src/main/java/org/mljames/aoc/aoc2024/day7/Day7Part1.java
+++ src/main/java/org/mljames/aoc/aoc2024/day7/Day7Part1.java
@@ -29,26 +29,30 @@ public class Day7Part1
 
             final List<String> allOperatorCombinations = possibleOperatorCombinations(numbers.size() - 1);
 
-            for (final String operatorCombination : allOperatorCombinations)
+            if (hasValidOperatorCombination(allOperatorCombinations, numbers, testValue, sumOfTestValues))
             {
-                BigInteger runningTotal = numbers.getFirst();
-                for (int j = 0; j < operatorCombination.length(); j++)
-                {
-                    if (runningTotal.compareTo(testValue) > 0)
-                    {
-                        break;
-                    }
-                    runningTotal = calculate(runningTotal, numbers.get(j + 1), operatorCombination.charAt(j));
-                }
-                if (runningTotal.compareTo(testValue) == 0)
-                {
-                    sumOfTestValues = sumOfTestValues.add(testValue);
-                    break;
-                }
+                sumOfTestValues = sumOfTestValues.add(testValue);
             }
         }
 
         LOGGER.info("The number of valid combinations is equal to: {}.", sumOfTestValues);
+    }
+
+    private static boolean hasValidOperatorCombination(List<String> allOperatorCombinations, List<BigInteger> numbers, BigInteger testValue, BigInteger sumOfTestValues)
+    {
+        for (final String operatorCombination : allOperatorCombinations)
+        {
+            BigInteger runningTotal = numbers.getFirst();
+            for (int j = 0; j < operatorCombination.length(); j++)
+            {
+                runningTotal = calculate(runningTotal, numbers.get(j + 1), operatorCombination.charAt(j));
+            }
+            if (runningTotal.compareTo(testValue) == 0)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
     private static BigInteger calculate(final BigInteger lhs, final BigInteger rhs, final char operator)
