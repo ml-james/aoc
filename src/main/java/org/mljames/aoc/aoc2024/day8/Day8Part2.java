bb49301afc954fc28ffb82930d3595bc9e917aea
matthewjames <matthew.james@lmax.com>
2024-12-08 11:16:56 -0500

    Add solution to Day8Part1

---
+++ src/main/java/org/mljames/aoc/aoc2024/day8/Day8Part2.java
@@ -0,0 +1,17 @@
+package org.mljames.aoc.aoc2024.day8;
+
+import org.mljames.aoc.PuzzleInputReader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+public class Day8Part2
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(Day8Part2.class);
+
+    public static void main(String[] args)
+    {
+        final List<String> input = PuzzleInputReader.readInput("aoc2024/day8/part2/puzzle_input.txt");
+    }
+}
